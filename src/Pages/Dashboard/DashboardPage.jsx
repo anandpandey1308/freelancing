fb43af066763380665f78163444249d6f32ad976
Anand Pandey <53857568+anandpandey1308@users.noreply.github.com>
2024-10-15 06:02:52 +0000

    test

--- src/Pages/Dashboard/DashboardPage.jsx
+++ src/Pages/Dashboard/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { dashboardConfig } from "./dashboardConfig";
-import SidebarItem from "./SidebarItem";
+import SidebarItem from "./SideBarItem";
 
 const DashboardPage = () => {
   const navigate = useNavigate();
